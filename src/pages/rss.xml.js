d42625949ecdcfffca79ed4e5839b9c74267e4dd
Ganessh Kumar R P <garp@microsoft.com>
2024-10-29 18:35:30 -0700

    fixing build issues

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,14 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
+  const items = await pagesGlobToRssItems(import.meta.glob('./articles/*.md'));
+  items.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+
   return rss({
     title: 'Ganessh Kumar',
     description: "Ganessh Kumar's personal site to write about technology and learnings",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./articles/*.md')),
+    items,
     customData: `<language>en-us</language>`,
   });
 }
